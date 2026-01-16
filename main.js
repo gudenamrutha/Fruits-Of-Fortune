895f8b64585de0e252b9eb59a3a1ba0a7a2a643f
gudenamrutha <namruthagude@gmail.com>
2026-01-16 16:04:04 +0530

    block color updated

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ const sounds = {
 //Creating Fake Symbols
 function createSymbol(type,color) {
     const symbol = new PIXI.Graphics();
-    symbol.beginFill(color);
+    symbol.beginFill(0xffffff);
     symbol.drawRoundedRect(0, 0, SYMBOL_SIZE, SYMBOL_SIZE, 16);
     symbol.endFill();
     symbol.type = type;
